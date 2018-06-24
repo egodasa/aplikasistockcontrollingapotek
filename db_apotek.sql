4bc7da97f2e70a70c36a855304e1923c03f42120
Ego Dafma Dasa <egodasa@gmail.com>
2018-06-24 11:43:04 +0700

    Perbaikan database tested

--- db_apotek.sql
+++ db_apotek.sql
@@ -1,58 +1,3 @@
--- Adminer 4.6.2 MySQL dump
-
-SET NAMES utf8;
-SET time_zone = '+00:00';
-SET foreign_key_checks = 0;
-SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
-
-DROP VIEW IF EXISTS `daftar_obat`;
-CREATE TABLE `daftar_obat` (`Id_Obat` int(11), `Nama_Obat` varchar(50), `Stok` int(11), `Id_Jobat` int(11), `Jenis_Obat` varchar(50), `Id_Sat_Obat` int(11), `Satuan` varchar(30), `Harga_Obat` int(11));
-
-
-DROP VIEW IF EXISTS `daftar_obat_beli`;
-CREATE TABLE `daftar_obat_beli` (`Id_Transaksi` varchar(50), `Id_Obat` int(11), `Nama_Obat` varchar(50), `Jumlah` decimal(32,0), `Harga_Obat` int(11), `Total_Harga` decimal(42,0));
-
-
-DROP VIEW IF EXISTS `daftar_pembelian`;
-CREATE TABLE `daftar_pembelian` (`Id_Pembelian` varchar(50), `Tanggal_Pembelian` varchar(72), `Jumlah` decimal(32,0), `Total_Harga` decimal(32,0));
-
-
-DROP VIEW IF EXISTS `daftar_satuan`;
-CREATE TABLE `daftar_satuan` (`Id_Sat` int(11), `Nama_Satuan` varchar(30), `Id_Jsat` int(11), `Nama_Jenis_Satuan` varchar(30));
-
-
-DROP VIEW IF EXISTS `daftar_transaksi`;
-CREATE TABLE `daftar_transaksi` (`Id_Transaksi` varchar(50), `Tanggal_Transaksi` varchar(72), `Jumlah` decimal(54,0), `Total_Harga` decimal(64,0));
-
-
-DROP VIEW IF EXISTS `laporan_detail_pembelian`;
-CREATE TABLE `laporan_detail_pembelian` (`id_obat` int(11), `id_detail` int(11), `id_pemasok` int(11), `id_pembelian` varchar(50), `Nama_Obat` varchar(50), `jumlah` int(11), `harga` int(11), `Satuan` varchar(30), `Nama_Pemasok` varchar(100));
-
-
-DROP VIEW IF EXISTS `laporan_detail_transaksi`;
-CREATE TABLE `laporan_detail_transaksi` (`Id_Transaksi` varchar(50), `Id_Obat` int(11), `Nama_Obat` varchar(50), `Harga_Obat` int(11), `Jumlah` int(11), `Total` bigint(21), `Satuan` varchar(30));
-
-
-DROP VIEW IF EXISTS `laporan_pemasok`;
-CREATE TABLE `laporan_pemasok` (`id_pemasok` int(11), `Nama_Pemasok` varchar(100), `Alamat` varchar(200), `Nomor_Telepon` varchar(20));
-
-
-DROP VIEW IF EXISTS `laporan_pemasukan`;
-CREATE TABLE `laporan_pemasukan` (`tgl_transaksi` datetime, `Id_Transaksi` varchar(50), `Nama_Obat` varchar(50), `Jumlah` decimal(32,0), `Total_Harga` decimal(42,0));
-
-
-DROP VIEW IF EXISTS `lap_bulanan`;
-CREATE TABLE `lap_bulanan` (`hari` int(2), `Jumlah` decimal(54,0), `Total_Harga` decimal(64,0));
-
-
-DROP VIEW IF EXISTS `lap_harian`;
-CREATE TABLE `lap_harian` (`tgl_transaksi` datetime, `Nama_Obat` varchar(50), `Jumlah` decimal(54,0), `Total_Harga` decimal(64,0));
-
-
-DROP VIEW IF EXISTS `lap_tahunan`;
-CREATE TABLE `lap_tahunan` (`bulan` int(2), `Jumlah` decimal(54,0), `Total_Harga` decimal(64,0));
-
-
 DROP TABLE IF EXISTS `tbl_detail_pembelian`;
 CREATE TABLE `tbl_detail_pembelian` (
   `id_detail` int(11) NOT NULL AUTO_INCREMENT,
@@ -70,15 +15,9 @@ INSERT INTO `tbl_detail_pembelian` (`id_detail`, `id_pembelian`, `id_obat`, `jum
 (6,	'2106181210346578',	9,	300,	1200000,	1)
 ON DUPLICATE KEY UPDATE `id_detail` = VALUES(`id_detail`), `id_pembelian` = VALUES(`id_pembelian`), `id_obat` = VALUES(`id_obat`), `jumlah` = VALUES(`jumlah`), `harga` = VALUES(`harga`), `id_pemasok` = VALUES(`id_pemasok`);
 
-DELIMITER ;;
+CREATE TRIGGER `tambah_stok_beli` AFTER INSERT ON `tbl_detail_pembelian` FOR EACH ROW update tbl_obat set stok = stok + NEW.jumlah where id_obat = NEW.id_obat;
 
-CREATE TRIGGER `tambah_stok_beli` AFTER INSERT ON `tbl_detail_pembelian` FOR EACH ROW
-update tbl_obat set stok = stok + NEW.jumlah where id_obat = NEW.id_obat;;
-
-CREATE TRIGGER `kurang_stok_beli` AFTER DELETE ON `tbl_detail_pembelian` FOR EACH ROW
-update tbl_obat set stok = stok - OLD.jumlah where id_obat = OLD.id_obat;;
-
-DELIMITER ;
+CREATE TRIGGER `kurang_stok_beli` AFTER DELETE ON `tbl_detail_pembelian` FOR EACH ROW update tbl_obat set stok = stok - OLD.jumlah where id_obat = OLD.id_obat;
 
 DROP TABLE IF EXISTS `tbl_detail_transaksi`;
 CREATE TABLE `tbl_detail_transaksi` (
@@ -102,15 +41,9 @@ INSERT INTO `tbl_detail_transaksi` (`id_detail`, `id_obat`, `jumlah`, `id_transa
 (14,	11,	5,	'2206180933141041')
 ON DUPLICATE KEY UPDATE `id_detail` = VALUES(`id_detail`), `id_obat` = VALUES(`id_obat`), `jumlah` = VALUES(`jumlah`), `id_transaksi` = VALUES(`id_transaksi`);
 
-DELIMITER ;;
+CREATE TRIGGER `kurangi_stok` AFTER INSERT ON `tbl_detail_transaksi` FOR EACH ROW update tbl_obat set stok = stok - new.jumlah where id_obat = new.id_obat;
 
-CREATE TRIGGER `kurangi_stok` AFTER INSERT ON `tbl_detail_transaksi` FOR EACH ROW
-update tbl_obat set stok = stok - new.jumlah where id_obat = new.id_obat;;
-
-CREATE TRIGGER `reset_stok` AFTER DELETE ON `tbl_detail_transaksi` FOR EACH ROW
-update tbl_obat set stok = stok + OLD.jumlah where id_obat = OLD.id_obat;;
-
-DELIMITER ;
+CREATE TRIGGER `reset_stok` AFTER DELETE ON `tbl_detail_transaksi` FOR EACH ROW update tbl_obat set stok = stok + OLD.jumlah where id_obat = OLD.id_obat;
 
 DROP TABLE IF EXISTS `tbl_jenis_obat`;
 CREATE TABLE `tbl_jenis_obat` (
@@ -259,40 +192,26 @@ INSERT INTO `tbl_transaksi` (`id_transaksi`, `tgl_transaksi`) VALUES
 ('2206180933141041',	'2018-06-22 09:34:17')
 ON DUPLICATE KEY UPDATE `id_transaksi` = VALUES(`id_transaksi`), `tgl_transaksi` = VALUES(`tgl_transaksi`);
 
-DROP TABLE IF EXISTS `daftar_obat`;
-CREATE ALGORITHM=UNDEFINED SQL SECURITY DEFINER VIEW `daftar_obat` AS select `a`.`id_obat` AS `Id_Obat`,`a`.`nm_obat` AS `Nama_Obat`,`a`.`stok` AS `Stok`,`b`.`id_jobat` AS `Id_Jobat`,`b`.`nm_jobat` AS `Jenis_Obat`,`a`.`id_sat_obat` AS `Id_Sat_Obat`,`c`.`Nama_Satuan` AS `Satuan`,`a`.`hrg_obat` AS `Harga_Obat` from ((`tbl_obat` `a` join `tbl_jenis_obat` `b` on((`a`.`id_jobat` = `b`.`id_jobat`))) join `daftar_satuan` `c` on((`a`.`id_sat_obat` = `c`.`Id_Sat`)));
+CREATE VIEW `daftar_satuan` AS select `a`.`id_sat` AS `Id_Sat`,`a`.`nm_sat` AS `Nama_Satuan`,`a`.`id_jsat` AS `Id_Jsat`,`b`.`nm_jsat` AS `Nama_Jenis_Satuan` from (`tbl_satuan` `a` join `tbl_jsatuan` `b` on((`a`.`id_jsat` = `b`.`id_jsat`)));
 
-DROP TABLE IF EXISTS `daftar_obat_beli`;
-CREATE ALGORITHM=UNDEFINED SQL SECURITY DEFINER VIEW `daftar_obat_beli` AS select `a`.`id_transaksi` AS `Id_Transaksi`,`a`.`id_obat` AS `Id_Obat`,`b`.`nm_obat` AS `Nama_Obat`,sum(`a`.`jumlah`) AS `Jumlah`,`b`.`hrg_obat` AS `Harga_Obat`,sum((`b`.`hrg_obat` * `a`.`jumlah`)) AS `Total_Harga` from (`tbl_detail_transaksi` `a` join `tbl_obat` `b` on((`a`.`id_obat` = `b`.`id_obat`))) group by `a`.`id_transaksi`,`a`.`id_obat`;
+CREATE VIEW `daftar_obat` AS select `a`.`id_obat` AS `Id_Obat`,`a`.`nm_obat` AS `Nama_Obat`,`a`.`stok` AS `Stok`,`b`.`id_jobat` AS `Id_Jobat`,`b`.`nm_jobat` AS `Jenis_Obat`,`a`.`id_sat_obat` AS `Id_Sat_Obat`,`c`.`Nama_Satuan` AS `Satuan`,`a`.`hrg_obat` AS `Harga_Obat` from ((`tbl_obat` `a` join `tbl_jenis_obat` `b` on((`a`.`id_jobat` = `b`.`id_jobat`))) join `daftar_satuan` `c` on((`a`.`id_sat_obat` = `c`.`Id_Sat`)));
 
-DROP TABLE IF EXISTS `daftar_pembelian`;
-CREATE ALGORITHM=UNDEFINED SQL SECURITY DEFINER VIEW `daftar_pembelian` AS select `b`.`id_pembelian` AS `Id_Pembelian`,date_format(`a`.`tgl_pembelian`,'%d-%M-%Y') AS `Tanggal_Pembelian`,sum(`b`.`jumlah`) AS `Jumlah`,sum(`b`.`harga`) AS `Total_Harga` from (`tbl_pembelian` `a` join `laporan_detail_pembelian` `b` on((`a`.`id_pembelian` = `b`.`id_pembelian`))) group by `a`.`id_pembelian` order by `a`.`tgl_pembelian` desc;
+CREATE VIEW `daftar_obat_beli` AS select `a`.`id_transaksi` AS `Id_Transaksi`,`a`.`id_obat` AS `Id_Obat`,`b`.`nm_obat` AS `Nama_Obat`,sum(`a`.`jumlah`) AS `Jumlah`,`b`.`hrg_obat` AS `Harga_Obat`,sum((`b`.`hrg_obat` * `a`.`jumlah`)) AS `Total_Harga` from (`tbl_detail_transaksi` `a` join `tbl_obat` `b` on((`a`.`id_obat` = `b`.`id_obat`))) group by `a`.`id_transaksi`,`a`.`id_obat`;
 
-DROP TABLE IF EXISTS `daftar_satuan`;
-CREATE ALGORITHM=UNDEFINED SQL SECURITY DEFINER VIEW `daftar_satuan` AS select `a`.`id_sat` AS `Id_Sat`,`a`.`nm_sat` AS `Nama_Satuan`,`a`.`id_jsat` AS `Id_Jsat`,`b`.`nm_jsat` AS `Nama_Jenis_Satuan` from (`tbl_satuan` `a` join `tbl_jsatuan` `b` on((`a`.`id_jsat` = `b`.`id_jsat`)));
+CREATE VIEW `laporan_pemasok` AS select `tbl_pemasok`.`id_pemasok` AS `id_pemasok`,`tbl_pemasok`.`nm_pemasok` AS `Nama_Pemasok`,`tbl_pemasok`.`alamat` AS `Alamat`,`tbl_pemasok`.`no_telpon` AS `Nomor_Telepon` from `tbl_pemasok`;
 
-DROP TABLE IF EXISTS `daftar_transaksi`;
-CREATE ALGORITHM=UNDEFINED SQL SECURITY DEFINER VIEW `daftar_transaksi` AS select `b`.`Id_Transaksi` AS `Id_Transaksi`,date_format(`a`.`tgl_transaksi`,'%d-%M-%Y') AS `Tanggal_Transaksi`,sum(`b`.`Jumlah`) AS `Jumlah`,sum(`b`.`Total_Harga`) AS `Total_Harga` from (`tbl_transaksi` `a` join `daftar_obat_beli` `b` on((`a`.`id_transaksi` = `b`.`Id_Transaksi`))) group by `a`.`id_transaksi` order by `a`.`tgl_transaksi` desc;
+CREATE VIEW `laporan_detail_pembelian` AS select `a`.`id_obat` AS `id_obat`,`a`.`id_detail` AS `id_detail`,`a`.`id_pemasok` AS `id_pemasok`,`a`.`id_pembelian` AS `id_pembelian`,`b`.`Nama_Obat` AS `Nama_Obat`,`a`.`jumlah` AS `jumlah`,`a`.`harga` AS `harga`,`b`.`Satuan` AS `Satuan`,`c`.`Nama_Pemasok` AS `Nama_Pemasok` from ((`tbl_detail_pembelian` `a` join `daftar_obat` `b` on((`a`.`id_obat` = `b`.`Id_Obat`))) join `laporan_pemasok` `c` on((`a`.`id_pemasok` = `c`.`id_pemasok`)));
 
-DROP TABLE IF EXISTS `laporan_detail_pembelian`;
-CREATE ALGORITHM=UNDEFINED SQL SECURITY DEFINER VIEW `laporan_detail_pembelian` AS select `a`.`id_obat` AS `id_obat`,`a`.`id_detail` AS `id_detail`,`a`.`id_pemasok` AS `id_pemasok`,`a`.`id_pembelian` AS `id_pembelian`,`b`.`Nama_Obat` AS `Nama_Obat`,`a`.`jumlah` AS `jumlah`,`a`.`harga` AS `harga`,`b`.`Satuan` AS `Satuan`,`c`.`Nama_Pemasok` AS `Nama_Pemasok` from ((`tbl_detail_pembelian` `a` join `daftar_obat` `b` on((`a`.`id_obat` = `b`.`Id_Obat`))) join `laporan_pemasok` `c` on((`a`.`id_pemasok` = `c`.`id_pemasok`)));
+CREATE VIEW `laporan_detail_transaksi` AS select `a`.`id_transaksi` AS `Id_Transaksi`,`a`.`id_obat` AS `Id_Obat`,`b`.`Nama_Obat` AS `Nama_Obat`,`b`.`Harga_Obat` AS `Harga_Obat`,`a`.`jumlah` AS `Jumlah`,(`a`.`jumlah` * `b`.`Harga_Obat`) AS `Total`,`b`.`Satuan` AS `Satuan` from (`tbl_detail_transaksi` `a` join `daftar_obat` `b` on((`a`.`id_obat` = `b`.`Id_Obat`)));
 
-DROP TABLE IF EXISTS `laporan_detail_transaksi`;
-CREATE ALGORITHM=UNDEFINED SQL SECURITY DEFINER VIEW `laporan_detail_transaksi` AS select `a`.`id_transaksi` AS `Id_Transaksi`,`a`.`id_obat` AS `Id_Obat`,`b`.`Nama_Obat` AS `Nama_Obat`,`b`.`Harga_Obat` AS `Harga_Obat`,`a`.`jumlah` AS `Jumlah`,(`a`.`jumlah` * `b`.`Harga_Obat`) AS `Total`,`b`.`Satuan` AS `Satuan` from (`tbl_detail_transaksi` `a` join `daftar_obat` `b` on((`a`.`id_obat` = `b`.`Id_Obat`)));
+CREATE VIEW `daftar_pembelian` AS select `b`.`id_pembelian` AS `Id_Pembelian`,date_format(`a`.`tgl_pembelian`,'%d-%M-%Y') AS `Tanggal_Pembelian`,sum(`b`.`jumlah`) AS `Jumlah`,sum(`b`.`harga`) AS `Total_Harga` from (`tbl_pembelian` `a` join `laporan_detail_pembelian` `b` on((`a`.`id_pembelian` = `b`.`id_pembelian`))) group by `a`.`id_pembelian` order by `a`.`tgl_pembelian` desc;
 
-DROP TABLE IF EXISTS `laporan_pemasok`;
-CREATE ALGORITHM=UNDEFINED SQL SECURITY DEFINER VIEW `laporan_pemasok` AS select `tbl_pemasok`.`id_pemasok` AS `id_pemasok`,`tbl_pemasok`.`nm_pemasok` AS `Nama_Pemasok`,`tbl_pemasok`.`alamat` AS `Alamat`,`tbl_pemasok`.`no_telpon` AS `Nomor_Telepon` from `tbl_pemasok`;
+CREATE VIEW `daftar_transaksi` AS select `b`.`Id_Transaksi` AS `Id_Transaksi`,date_format(`a`.`tgl_transaksi`,'%d-%M-%Y') AS `Tanggal_Transaksi`,sum(`b`.`Jumlah`) AS `Jumlah`,sum(`b`.`Total_Harga`) AS `Total_Harga` from (`tbl_transaksi` `a` join `daftar_obat_beli` `b` on((`a`.`id_transaksi` = `b`.`Id_Transaksi`))) group by `a`.`id_transaksi` order by `a`.`tgl_transaksi` desc;
 
-DROP TABLE IF EXISTS `laporan_pemasukan`;
-CREATE ALGORITHM=UNDEFINED SQL SECURITY DEFINER VIEW `laporan_pemasukan` AS select `a`.`tgl_transaksi` AS `tgl_transaksi`,`b`.`Id_Transaksi` AS `Id_Transaksi`,`b`.`Nama_Obat` AS `Nama_Obat`,`b`.`Jumlah` AS `Jumlah`,`b`.`Total_Harga` AS `Total_Harga` from (`daftar_obat_beli` `b` join `tbl_transaksi` `a` on((`a`.`id_transaksi` = `b`.`Id_Transaksi`)));
+CREATE VIEW `laporan_pemasukan` AS select `a`.`tgl_transaksi` AS `tgl_transaksi`,`b`.`Id_Transaksi` AS `Id_Transaksi`,`b`.`Nama_Obat` AS `Nama_Obat`,`b`.`Jumlah` AS `Jumlah`,`b`.`Total_Harga` AS `Total_Harga` from (`daftar_obat_beli` `b` join `tbl_transaksi` `a` on((`a`.`id_transaksi` = `b`.`Id_Transaksi`)));
 
-DROP TABLE IF EXISTS `lap_bulanan`;
-CREATE ALGORITHM=UNDEFINED SQL SECURITY DEFINER VIEW `lap_bulanan` AS select dayofmonth(`laporan_pemasukan`.`tgl_transaksi`) AS `hari`,sum(`laporan_pemasukan`.`Jumlah`) AS `Jumlah`,sum(`laporan_pemasukan`.`Total_Harga`) AS `Total_Harga` from `laporan_pemasukan` where ((month(`laporan_pemasukan`.`tgl_transaksi`) = month(now())) and (year(`laporan_pemasukan`.`tgl_transaksi`) = year(now()))) group by dayofmonth(`laporan_pemasukan`.`tgl_transaksi`);
+CREATE VIEW `lap_harian` AS select `laporan_pemasukan`.`tgl_transaksi` AS `tgl_transaksi`,`laporan_pemasukan`.`Nama_Obat` AS `Nama_Obat`,sum(`laporan_pemasukan`.`Jumlah`) AS `Jumlah`,sum(`laporan_pemasukan`.`Total_Harga`) AS `Total_Harga` from `laporan_pemasukan` where date(`laporan_pemasukan`.`tgl_transaksi`) = date(now()) group by `laporan_pemasukan`.`Nama_Obat`;
 
-DROP TABLE IF EXISTS `lap_harian`;
-CREATE ALGORITHM=UNDEFINED SQL SECURITY DEFINER VIEW `lap_harian` AS select `laporan_pemasukan`.`tgl_transaksi` AS `tgl_transaksi`,`laporan_pemasukan`.`Nama_Obat` AS `Nama_Obat`,sum(`laporan_pemasukan`.`Jumlah`) AS `Jumlah`,sum(`laporan_pemasukan`.`Total_Harga`) AS `Total_Harga` from `laporan_pemasukan` group by `laporan_pemasukan`.`Nama_Obat`;
+CREATE VIEW `lap_bulanan` AS select dayofmonth(`laporan_pemasukan`.`tgl_transaksi`) AS `hari`,sum(`laporan_pemasukan`.`Jumlah`) AS `Jumlah`,sum(`laporan_pemasukan`.`Total_Harga`) AS `Total_Harga` from `laporan_pemasukan` where ((month(`laporan_pemasukan`.`tgl_transaksi`) = month(now())) and (year(`laporan_pemasukan`.`tgl_transaksi`) = year(now()))) group by dayofmonth(`laporan_pemasukan`.`tgl_transaksi`);
 
-DROP TABLE IF EXISTS `lap_tahunan`;
-CREATE ALGORITHM=UNDEFINED SQL SECURITY DEFINER VIEW `lap_tahunan` AS select month(`laporan_pemasukan`.`tgl_transaksi`) AS `bulan`,sum(`laporan_pemasukan`.`Jumlah`) AS `Jumlah`,sum(`laporan_pemasukan`.`Total_Harga`) AS `Total_Harga` from `laporan_pemasukan` where (year(`laporan_pemasukan`.`tgl_transaksi`) = year(now())) group by month(`laporan_pemasukan`.`tgl_transaksi`);
-
--- 2018-06-22 13:43:59
+CREATE VIEW `lap_tahunan` AS select month(`laporan_pemasukan`.`tgl_transaksi`) AS `bulan`,sum(`laporan_pemasukan`.`Jumlah`) AS `Jumlah`,sum(`laporan_pemasukan`.`Total_Harga`) AS `Total_Harga` from `laporan_pemasukan` where (year(`laporan_pemasukan`.`tgl_transaksi`) = year(now())) group by month(`laporan_pemasukan`.`tgl_transaksi`);
